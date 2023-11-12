bc1a14531f2a008252350263bf46b4f644fd307b
rizalpahlevii <mrizalpahlevi372@gmail.com>
2023-11-12 08:24:33 +0700

    add some env

--- config/database.go
+++ config/database.go
@@ -17,8 +17,8 @@ func handleDatabaseConnection() {
 	//err := godotenv.Load()
 	//helper.PanicIfError(err)
 
-	host := "postgres"
-	port := "5423"
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
